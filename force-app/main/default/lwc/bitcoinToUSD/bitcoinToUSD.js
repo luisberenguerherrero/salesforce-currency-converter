14cd5523092dbfae0d19ecb35bb9535d8ebd546f
Luis Berenguer <luisberenguer96@gmail.com>
2019-08-29 20:40:51 +0200

    Added some logic
    test
    
    
    Added comments and logic to Apex classes

--- force-app/main/default/lwc/bitcoinToUSD/bitcoinToUSD.js
+++ force-app/main/default/lwc/bitcoinToUSD/bitcoinToUSD.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from "lwc";
+import getOneBitcoinUSDValue from "@salesforce/apex/BitcoinToUSDConvertor.getValueFromBitpay";
 
-export default class BitcoinToUSD extends LightningElement {}
+export default class BitcoinToUSD extends LightningElement {
+  @wire(getOneBitcoinUSDValue)
+  wiredValue({ error, data }) {
+    if (data) {
+      this.oneBitcoinUSDValue = data;
+      this.error = undefined;
+    } else if (error) {
+      this.error = 'Unknown error';
+      if (Array.isArray(error.body)) {
+        this.error = error.body.map(e => e.message).join(', ');
+      } else if (typeof error.body.message === 'string') {
+        this.error = error.body.message;
+      }
+    }
+  }
+  @track oneBitcoinUSDValue;
+  @track error;
+  @track bitcoinValue = 1;
+  @track usdValue = this.oneBitcoinUSDValue;
+
+  handleUSDChange(event) {
+    let usdInput = event.target.value;
+    this.usdValue = usdInput;
+    this.bitcoinValue = usdInput / this.oneBitcoinUSDValue;
+  }
+
+  handleBitcoinChange(event) {
+    let bitcoinInput = event.target.value;
+    this.bitcoinValue = bitcoinInput;
+    this.usdValue = bitcoinInput * this.oneBitcoinUSDValue;
+  }
+
+  get oneUSDBitcoinValue() {
+    return 1 / this.oneBitcoinUSDValue;
+  }
+}
--- force-app/main/default/lwc/createOpportunities/createOpportunities.js
+++ force-app/main/default/lwc/createOpportunities/createOpportunities.js
@@ -1,3 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from "lwc";
+import getAccounts from "@salesforce/apex/CreateOpportunitiesController.getAccounts";
 
-export default class CreateOpportunities extends LightningElement {}
+const accountsPerPage = 5;
+
+export default class CreateOpportunities extends LightningElement {
+  @track page = 1;
+  @track selectedAccounts = [];
+  //@wire(getAccounts, {page: '$page', records: accountsPerPage}) accounts;
+
+  selectAccount(event) {
+    let selectedAccount = this.accounts.find(
+      x => x.Id === event.target.dataset.item
+    );
+    this.selectedAccounts.push(selectedAccount);
+  }
+
+  removeSelectedAccount(event) {
+    let indexToRemove = this.selectedAccounts.findIndex(
+      x => x.Id === event.target.dataset.item
+    );
+    this.selectedAccounts.splice(indexToRemove);
+  }
+
+  first() {}
+  back() {}
+  next() {}
+  last() {}
+
+  disabledBackwardsButtons() {
+    return this.page === 1;
+  }
+  disabledForwardButtons() {
+    return this.page;
+  }
+}
