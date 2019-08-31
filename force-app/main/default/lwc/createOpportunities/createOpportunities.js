bf028f5d620c65553a91e26ef08cd18b5b4d8543
Luis Berenguer <luisberenguer96@gmail.com>
2019-08-31 13:09:58 +0200

    Create Opportunities Layout

--- force-app/main/default/lwc/createOpportunities/createOpportunities.js
+++ force-app/main/default/lwc/createOpportunities/createOpportunities.js
@@ -3,10 +3,68 @@ import getAccounts from "@salesforce/apex/CreateOpportunitiesController.getAccou
 
 const accountsPerPage = 5;
 
+const columns = [
+  { label: 'Account Name', fieldName: 'name' }/* ,
+  { label: 'Website', fieldName: 'website', type: 'url' },
+  { label: 'Phone', fieldName: 'phone', type: 'phone' },
+  { label: 'Balance', fieldName: 'amount', type: 'currency' },
+  { label: 'CloseAt', fieldName: 'closeAt', type: 'date' }, */
+];
+
 export default class CreateOpportunities extends LightningElement {
   @track page = 1;
   @track selectedAccounts = [];
   //@wire(getAccounts, {page: '$page', records: accountsPerPage}) accounts;
+
+
+    @track data = [{
+      id: 'a',
+      name: 'name fro maugustus quo throug north quay london brie cheese name fro maugustus quo throug north quay london brie cheese name fro maugustus quo throug north quay london brie cheese',
+      email: 'email',
+      website: 'url',
+      amount: 'currency',
+      phone: 'phoneNumber',
+      closeAt: 'dateInFuture'
+    },{
+      id: 'b',
+      name: 'name',
+      email: 'email',
+      website: 'url',
+      amount: 'currency',
+      phone: 'phoneNumber',
+      closeAt: 'dateInFuture'
+    },{
+      id: 'c',
+      name: 'name',
+      email: 'email',
+      website: 'url',
+      amount: 'currency',
+      phone: 'phoneNumber',
+      closeAt: 'dateInFuture'
+    },{
+      id: 'd',
+      name: 'name',
+      email: 'email',
+      website: 'url',
+      amount: 'currency',
+      phone: 'phoneNumber',
+      closeAt: 'dateInFuture'
+    },{
+      id: 'e',
+      name: 'name',
+      email: 'email',
+      website: 'url',
+      amount: 'currency',
+      phone: 'phoneNumber',
+      closeAt: 'dateInFuture'
+    }];
+    @track columns = columns;
+
+    @track selectedAccounts = new Map();
+
+    get selectedRows(){
+      return [ ...this.selectedAccounts.keys() ];
+    }
 
   selectAccount(event) {
     let selectedAccount = this.accounts.find(
@@ -27,7 +85,7 @@ export default class CreateOpportunities extends LightningElement {
   next() {}
   last() {}
 
-  disabledBackwardsButtons() {
+  disabledBackwardButtons() {
     return this.page === 1;
   }
   disabledForwardButtons() {
