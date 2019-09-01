8866c0ea7f66aea38bf7090fc1a8d2794bb9e872
Luis Berenguer Herrero <luisberenguer96@gmail.com>
2019-09-02 00:19:58 +0200

    Selection on Accounts WIP

--- force-app/main/default/lwc/createOpportunities/createOpportunities.js
+++ force-app/main/default/lwc/createOpportunities/createOpportunities.js
@@ -2,93 +2,100 @@ import { LightningElement, track, wire } from "lwc";
 import getAccounts from "@salesforce/apex/CreateOpportunitiesController.getAccounts";
 
 const accountsPerPage = 5;
-
-const columns = [
-  { label: 'Account Name', fieldName: 'name' }/* ,
-  { label: 'Website', fieldName: 'website', type: 'url' },
-  { label: 'Phone', fieldName: 'phone', type: 'phone' },
-  { label: 'Balance', fieldName: 'amount', type: 'currency' },
-  { label: 'CloseAt', fieldName: 'closeAt', type: 'date' }, */
-];
+const columns = [{ label: "Account Name", fieldName: "Name" }];
 
 export default class CreateOpportunities extends LightningElement {
+  columns = columns;
   @track page = 1;
-  @track selectedAccounts = [];
-  //@wire(getAccounts, {page: '$page', records: accountsPerPage}) accounts;
+  @track selectedAccountsMap = new Map();
+  @track querySearch;
+  @track accounts = [];
+  @track number_pages;
 
+  @track opportunityData = {};
 
-    @track data = [{
-      id: 'a',
-      name: 'name fro maugustus quo throug north quay london brie cheese name fro maugustus quo throug north quay london brie cheese name fro maugustus quo throug north quay london brie cheese',
-      email: 'email',
-      website: 'url',
-      amount: 'currency',
-      phone: 'phoneNumber',
-      closeAt: 'dateInFuture'
-    },{
-      id: 'b',
-      name: 'name',
-      email: 'email',
-      website: 'url',
-      amount: 'currency',
-      phone: 'phoneNumber',
-      closeAt: 'dateInFuture'
-    },{
-      id: 'c',
-      name: 'name',
-      email: 'email',
-      website: 'url',
-      amount: 'currency',
-      phone: 'phoneNumber',
-      closeAt: 'dateInFuture'
-    },{
-      id: 'd',
-      name: 'name',
-      email: 'email',
-      website: 'url',
-      amount: 'currency',
-      phone: 'phoneNumber',
-      closeAt: 'dateInFuture'
-    },{
-      id: 'e',
-      name: 'name',
-      email: 'email',
-      website: 'url',
-      amount: 'currency',
-      phone: 'phoneNumber',
-      closeAt: 'dateInFuture'
-    }];
-    @track columns = columns;
-
-    @track selectedAccounts = new Map();
-
-    get selectedRows(){
-      return [ ...this.selectedAccounts.keys() ];
+  @wire(getAccounts, {
+    page: '$page',
+    recordsPerPage: accountsPerPage,
+    querySearch: ''
+  })
+  wiredValue({ error, data }) {
+    if (data) {
+      this.accounts = data.accounts;
+      this.number_pages = data.number_pages;
+    } else if (error) {
+      console.error(error);
     }
+  }
+
+  @track selectedRows = [];
+  @track selectedAccounts = [];
+/*   get selectedRows() {
+     console.log('Selected rows: ' + [...this.selectedAccountsMap.keys()]);
+   return [...this.selectedAccountsMap.keys()];
+  }
+  get selectedAccounts() {
+    console.log('Selected accounts: ' + [...this.selectedAccountsMap.values()]);
+    return [...this.selectedAccountsMap.values()];
+  } */
+
+  handleChange(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+    console.log("Name: " + name);
+    console.log("Value: " + value);
+    this.opportunityData[name] = value;
+  }
+
+  handleRemoveEvent(event) {
+    console.log("Remove account from child component with id " + event.detail);
+    this.selectedAccountsMap.delete(event.detail);
+    this.selectedRows=[...this.selectedAccountsMap.keys()];
+    this.selectedAccounts=[...this.selectedAccountsMap.values()];
+  }
 
   selectAccount(event) {
-    let selectedAccount = this.accounts.find(
-      x => x.Id === event.target.dataset.item
-    );
-    this.selectedAccounts.push(selectedAccount);
+    const selectedRows = event.detail.selectedRows;
+    // Display that fieldName of the selected rows
+    let mapSelecteds = new Map();
+    for (const row of selectedRows) {
+
+        console.log("Selected "+row.Name+" with id " + row.Id);
+        let selectedAccount = this.accounts.find(
+          x => x.Id === row.Id
+        );
+        mapSelecteds.set(selectedAccount.Id, selectedAccount);
+        console.log(this.selectedAccountsMap.size);
+
+    }
+    this.selectedAccountsMap=mapSelecteds;
+    this.selectedRows=[...this.selectedAccountsMap.keys()];
+    this.selectedAccounts=[...this.selectedAccountsMap.values()];
+    console.log(JSON.parse(JSON.stringify(this.selectedAccounts)));
   }
 
   removeSelectedAccount(event) {
-    let indexToRemove = this.selectedAccounts.findIndex(
-      x => x.Id === event.target.dataset.item
-    );
-    this.selectedAccounts.splice(indexToRemove);
+    console.log("Remove account with id " + event.target.dataset.item);
+    this.selectedAccountsMap.delete(event.target.dataset.item);
   }
 
-  first() {}
-  back() {}
-  next() {}
-  last() {}
+  first() {
+    this.page = 1;
+  }
+  back() {
+    this.page--;
+  }
+  next() {
+    this.page++;
+  }
+  last() {
+    this.page = this.number_pages;
+  }
 
-  disabledBackwardButtons() {
+  get disabledBackwardButtons() {
     return this.page === 1;
   }
-  disabledForwardButtons() {
-    return this.page;
+  get disabledForwardButtons() {
+    return this.page === this.number_pages;
   }
 }
--- force-app/main/default/lwc/previewOpportunities/previewOpportunities.js
+++ force-app/main/default/lwc/previewOpportunities/previewOpportunities.js
@@ -1,3 +1,50 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from "lwc";
 
-export default class PreviewOpportunities extends LightningElement {}
+export default class PreviewOpportunities extends LightningElement {
+    @track opportunitiesPreview = [];
+    @api pageChanged;
+    @api opportunityData;
+    @api
+    get accounts() {
+        return this._accounts;
+    }
+    //Every time new parameters are received, we load Policies from the ICVC
+    set accounts(value) {
+      console.log('updated value');
+      console.log(JSON.parse(JSON.stringify(value)));
+        this._accounts = value;
+        if (this._accounts != null) {
+            let newOpportunities = [];
+            for (const account of this._accounts) {
+                newOpportunities.push({
+                    AccountId: account.Id,
+                    Name: "New auto opportunity created for " + account.Name,
+                    Amount: this.opportunityData.amount,
+                    Stage: this.opportunityData.stage,
+                    CloseDate: this.opportunityData.date
+                });
+
+            }
+            this.opportunitiesPreview=newOpportunities;
+        }
+        console.log(JSON.parse(JSON.stringify(this.opportunitiesPreview)));
+
+    }
+
+
+  removeAccount(event) {
+    const removeEvent = new CustomEvent("remove", {
+      detail: event.target.dataset.item
+    });
+    this.dispatchEvent(removeEvent);
+  }
+
+  handleClick() {
+    //handle creation
+    //on creation, show a pop up with details (% on success inserts, and fail details)
+
+    //on close pop up, remain bad insertions with their error message and a button to retry insert
+
+    //if all success, empty selected accounts
+  }
+}
